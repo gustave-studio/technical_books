7f1121604dbb7dc5ce7850cd8ce6579e480d3c95
gustave-studio <gustave0222@gmail.com>
2021-12-19 23:17:41 +0900

    ✨フッター追加

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = function () {
+  return (
+    <div className="footer">
+      Copyright 2021 Gustave Studio(ギュスターヴ スタジオ)
+    </div>
+  );
+};
+
+export default Footer;
