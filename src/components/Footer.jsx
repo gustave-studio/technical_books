c109363b041f15e3d85518ac6dc2d536a4fce101
gustave-studio <gustave0222@gmail.com>
2022-01-02 02:34:06 +0900

    📝2022に変更

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = function () {
   return (
     <div className="footer">
-      ©︎ Copyright 2021 Gustave Studio(ギュスターヴ スタジオ)
+      ©︎ Copyright 2022&nbsp;
+      <a href="http://gustave-studio.com/" target="_blank" rel="noreferrer">
+        Gustave Studio(ギュスターヴ スタジオ)
+      </a>
     </div>
   );
 };
